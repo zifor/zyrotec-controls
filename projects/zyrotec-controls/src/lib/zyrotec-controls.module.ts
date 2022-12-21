87e7897114554d2a333a31d11b0761a59465d80c
zyrotec <louisno28@gmail.com>
2022-12-22 01:17:36 +0200

    created library and application

---
+++ projects/zyrotec-controls/src/lib/zyrotec-controls.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { ZyrotecControlsComponent } from './zyrotec-controls.component';
+
+
+
+@NgModule({
+  declarations: [
+    ZyrotecControlsComponent
+  ],
+  imports: [
+  ],
+  exports: [
+    ZyrotecControlsComponent
+  ]
+})
+export class ZyrotecControlsModule { }
---
+++ projects/zyrotec-controls/src/public-api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of zyrotec-controls
+ */
+
+export * from './lib/zyrotec-controls.service';
+export * from './lib/zyrotec-controls.component';
+export * from './lib/zyrotec-controls.module';
